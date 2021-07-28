a88f58b5c9d7632e8a1a1c5ba4bd7f27e6daf3cf
TheGaky <kuznetsoff.artemiy@gmail.com>
2021-07-28 17:13:34 +0300

    improve center and fee parsers

--- keyboards/inline/center.py
+++ keyboards/inline/center.py
@@ -5,7 +5,11 @@ r = requests.get(url)
 r.encoding = 'utf-8-sig'
 from bs4 import BeautifulSoup
 data = BeautifulSoup(r.text, features='html.parser')
-center_c = []
+center_c = ""
 center = data.find_all("div", {"class": "article_body"})
 for i in center:
-    center_c.append(i)
+    try:
+        center_c+=i.text
+    except:
+        pass
+print(center_c)
--- keyboards/inline/fee.py
+++ keyboards/inline/fee.py
@@ -5,7 +5,11 @@ r = requests.get(url)
 r.encoding = 'utf-8-sig'
 from bs4 import BeautifulSoup
 data = BeautifulSoup(r.text, features='html.parser')
-fee_list = []
+fee_list = ""
 fee_content = data.find_all("div", {"class": "article_body"})
 for i in fee_content:
-    fee_list.append(i)
+    try:
+        fee_list+=i.text
+    except:
+        pass
+
