0523e7a5783004c531e1f480bc987053c846b4d5
TheGaky <kuznetsoff.artemiy@gmail.com>
2021-07-28 16:37:56 +0300

    impove war parse

--- keyboards/inline/war_parser.py
+++ keyboards/inline/war_parser.py
@@ -5,11 +5,17 @@ r = requests.get("https://www.mzv.cz/moscow/ru/soobschenia_sobytija/x2020_04_03/
 r.encoding = 'utf-8'
 from bs4 import BeautifulSoup
 data = BeautifulSoup(r.text, features='html.parser')
-c = []
+war_content = ''
 content = data.find_all("p", {"class": "article_perex"})
 content1 = data.find_all("div", {"class": "article_body"})
-g = data.find_all("p",{"class": "article_perex"}, {"div"})
 for i in content:
-    c.append(i)
+    try:
+        war_content+=i.text
+    except:
+        pass
 for i in content1:
-    c.append(i)
+    try:
+        war_content+=i.text
+    except:
+        pass
+print(war_content)
