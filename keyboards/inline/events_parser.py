bddcf1613c52920b91e5bffa697191ce019c19f7
Gaky <kuznetsoff.artemiy@gmail.com>
2021-08-06 17:37:28 +0300

    ded

--- keyboards/inline/events_parser.py
+++ keyboards/inline/events_parser.py
@@ -5,10 +5,14 @@ r = requests.get("https://www.mzv.cz/moscow/ru/vizy_i_konsulskaja/index.html")
 r.encoding = 'utf-8-sig'
 from bs4 import BeautifulSoup
 data = BeautifulSoup(r.text, features='html.parser')
-content = data.find_all("h2", {"class": "article_title"})
-events = {}
+content = data.find_all("div", {"class": "article_content"})
+events = ''
 for i in content:
-    events[i.getText()] = "https://www.mzv.cz" + i.a["href"]
+    try:
+        events+=i.text
+    except:
+        pass
 
 
 
+print(events)
