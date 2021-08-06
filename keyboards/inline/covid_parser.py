00932a9bd86d5681e34873f6575155f9db972b8a
Gaky <kuznetsoff.artemiy@gmail.com>
2021-08-06 21:36:47 +0300

    made telegraph for covid

--- keyboards/inline/covid_parser.py
+++ keyboards/inline/covid_parser.py
@@ -1,17 +1,18 @@
 import requests
-
+from telegraph_page_maker import create_page
 
 r = requests.get("https://www.mzv.cz/moscow/ru/vizy_i_konsulskaja/covid_19/index.html")
 r.encoding = 'utf-8-sig'
 from bs4 import BeautifulSoup
 data = BeautifulSoup(r.text, features='html.parser')
 content = data.find_all("h2", {"class": "article_title"})
+title = data.find("h1", {"class": "article_title"})
 list5 = {}
+d = create_page(title.text, "\n".join(content.split("\n")[1:-1]))
 for i in content:
-    list5[i.getText()] = "https://www.mzv.cz" + i.a["href"]
+    list5[i.getText()] = d
 
-
-def get_covid_article(num):
+"""def get_covid_article(num):
     url = list(list5.values())[num-1]
     r = requests.get(url)
     r.encoding = 'utf-8-sig'
@@ -25,4 +26,4 @@ def get_covid_article(num):
         except:
             pass
 
-    return article_content
+    return article_content"""
